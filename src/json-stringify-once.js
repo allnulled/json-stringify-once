b8aec2faca75a4d6445806756e6fd467dfee8ff0
allnulled <todosanulados@gmail.com>
2018-09-02 17:28:06 +0200

    Updated whole module with docs included.

--- src/json-stringify-once.js
+++ src/json-stringify-once.js
@@ -4,6 +4,43 @@
 // From awesome answer at: 
 // https://stackoverflow.com/questions/11616630/json-stringify-avoid-typeerror-converting-circular-structure-to-json
 
+/**
+ *
+ * # json-stringify-once
+ * 
+ * JSON stringify without `Converting circular structure to JSON` error.
+ * 
+ * Note: this snippet was extracted from an awesome answer at StakOverflow, from [here](https://stackoverflow.com/questions/11616630/json-stringify-avoid-typeerror-converting-circular-structure-to-json) to be honest. Thanks to the users that posted, edited and helped to give this answer ([1](https://stackoverflow.com/users/1068746/guy-mograbi), [2](https://stackoverflow.com/users/912236/orwellophile) and [3](https://stackoverflow.com/users/371698/isak)).*
+ * 
+ * ## 1. Installation
+ * 
+ * ~$ `npm install --save json-stringify-once`
+ * 
+ * ## 2. Usage
+ * 
+ * First, you need to import the module from your code:
+ * 
+ * ```js
+ * const stringify = require("json-stringify-once");
+ * ```
+ * 
+ * Once you have added the code into your project, you will be able to:
+ * 
+ * ```js
+ * JSON.stringifyOnce(window); // not formatted
+ * JSON.stringifyOnce(window, null, 4); // formatted
+ * stringify(window); // not formatted
+ * stringify(window, null, 4); // formatted
+ * ```
+ * 
+ * Notice that the method used is not `stringify`, but `stringifyOnce`.
+ * 
+ * ## 3. Conclusion
+ * 
+ * There are lots of awesome codes in StackOverflow. Lots of them.
+ * 
+ */
+
 JSON.stringifyOnce = function(obj, replacer, indent){
     var printedObjects = [];
     var printedObjectKeys = [];
